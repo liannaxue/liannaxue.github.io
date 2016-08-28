27674499b15da17ecd1f258a50f719d9250a5e1b
liannaxue <liannaxue0618@outlook.com>
2016-08-27 22:24:20 -0400

    homework

---
+++ class11/script.js
@@ -0,0 +1,8 @@
+		function turnOn() {
+			document.getElementById('Bobby').src= 'pic_bulbon.gif';
+		}
+
+
+		function turnOff() {
+			document.getElementById('Bobby').src= 'pic_bulboff.gif';
+		}		
